aeea2f9cef26952194b964654ede2673e15abe64
Luiz A D Silva <luizbha@hotmail.com>
2019-10-08 17:10:44 -0300

    Curso Vue.js aula02 exercicio02

---
+++ projetos/projeto1/src/components/Titulo.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <!-- <h1 v-bind:style="{color: cor}">{{titulo}}</h1> -->
+  <h1 :style="{color: cor}">{{titulo}}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  
+    props: ['titulo', 'cor']
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ projetos/projeto1/src/components/tabela.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <titulo titulo="Curso Vue.JS - Aula 02 - Exercicio 2" cor="red"></titulo>   
+   <table id="styletabela" border="1">
+  <tr>
+    <th>Nome</th>
+    <th>Cargo</th>
+    <th>Unidade</th>
+  </tr>
+  <tr>
+    <td>José</td>
+    <td>Analista</td>
+    <td>Tupis</td>
+  </tr>
+  <tr>
+    <td>Maria</td>
+    <td>Gerente</td>
+    <td>Sede</td>
+  </tr>
+</table> 
+</div>
+</template>
+
+<script>
+export default {      
+
+}
+</script>
+
+<style>
+  #styletabela{
+    width: 100%;    
+    text-align: center;
+    background-color:cornsilk;
+    color: green;
+    
+  }
+</style>
